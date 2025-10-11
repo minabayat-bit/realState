e68426e1b8af087bf500070aa6082326da4a550e
m.bayat <minabayatt9@gmail.com>
2025-10-11 21:00:53 +0330

    v1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+
 import './style.css'
+
+const hiddenEL = document.querySelectorAll|(".hiddenitem")
+
+const slidersEL = document.getElementById("sliders")
+
+const getSlide = async ()=>{
+    const res = await fetch("https://675ec6a21f7ad24269969bf9.mockapi.io/real-state/real-state");
+    const data = await res.json
+    console.log(data)
+}
+getSlide()
